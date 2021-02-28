85bc5bd9ba8e0f694af65ea21f1ad841067c11de
Tim Bernhard <tim@bernhard-webstudio.ch>
2021-02-28 13:34:01 +0100

    Update sh to reflect actual changes

--- bin/generateSDK.sh
+++ bin/generateSDK.sh
@@ -6,40 +6,48 @@ cd "$(dirname "$0")/.." || exit
 # delete the currrent API
 rm -rf src
 
-# then, clone the sources
-mkdir apiary-src
-cd apiary-src || exit
-if ! [ -d "doc-api-pipe" ]; then
-  git clone git@github.com:pipefy/doc-api-pipe.git
-fi
-if ! [ -d "doc-api-general" ]; then
-  git clone git@github.com:pipefy/doc-api-general.git
-fi
-if ! [ -d "doc-api-database" ]; then
-  git clone https://github.com/pipefy/doc-api-database
-fi
+## MODERN WAY:
+yarn install;
+composer install;
 
-# then, convert the API Blueprint to Swagger
-pwd
-for DIRECTORY in ./*; do
-  if [ -d "$DIRECTORY" ]; then
-    cd "$DIRECTORY" || (echo "Could not switch directory to $DIRECTORY" && exit 2)
-    npx apib2swagger --bearer-apikey -i ./*.apib -o ./swagger.json
-    cd ..
-  fi
-done
+yarn graphdoc -e https://api.pipefy.com/graphql -x "Authorization: Bearer $GRAPHQL_TOKEN" -p graphdoc-schema-json-plugin -o ./doc/schema
+php ./bin/generatePhpApi.php
 
-pwd
-# finally, use Swagger Codegen to generate the API
-for DIRECTORY in ./*; do
-  if [ -d "$DIRECTORY" ]; then
-    cd "$DIRECTORY" || (echo "Could not switch directory to $DIRECTORY" && exit 3)
-    DIRECTORY_NAME=$(basename "$DIRECTORY")
-    swagger-codegen generate -c ../../bin/generatePhpConfig.json -i ./swagger.json -l php -o "../../src/$DIRECTORY_NAME"
-    cd ..
-  fi
-done
+## LEGACY WAY:
+# # then, clone the sources
+# mkdir apiary-src
+# cd apiary-src || exit
+# if ! [ -d "doc-api-pipe" ]; then
+#   git clone git@github.com:pipefy/doc-api-pipe.git
+# fi
+# if ! [ -d "doc-api-general" ]; then
+#   git clone git@github.com:pipefy/doc-api-general.git
+# fi
+# if ! [ -d "doc-api-database" ]; then
+#   git clone https://github.com/pipefy/doc-api-database
+# fi
 
-pwd
-cd ..
-rm -rf apiary-src
+# # then, convert the API Blueprint to Swagger
+# pwd
+# for DIRECTORY in ./*; do
+#   if [ -d "$DIRECTORY" ]; then
+#     cd "$DIRECTORY" || (echo "Could not switch directory to $DIRECTORY" && exit 2)
+#     npx apib2swagger --bearer-apikey -i ./*.apib -o ./swagger.json
+#     cd ..
+#   fi
+# done
+
+# pwd
+# # finally, use Swagger Codegen to generate the API
+# for DIRECTORY in ./*; do
+#   if [ -d "$DIRECTORY" ]; then
+#     cd "$DIRECTORY" || (echo "Could not switch directory to $DIRECTORY" && exit 3)
+#     DIRECTORY_NAME=$(basename "$DIRECTORY")
+#     swagger-codegen generate -c ../../bin/generatePhpConfig.json -i ./swagger.json -l php -o "../../src/$DIRECTORY_NAME"
+#     cd ..
+#   fi
+# done
+
+# pwd
+# cd ..
+# rm -rf apiary-src
